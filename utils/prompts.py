67dd82e06777d4c824466cf260254c5d3f67de32
Matias Bazzano <matiasbazzano19@gmail.com>
2025-10-10 15:07:05 -0300

    Update prompts.py

--- utils/prompts.py
+++ utils/prompts.py
@@ -2,7 +2,7 @@ from langchain.prompts import PromptTemplate
 
 AGENT_PROMPT = PromptTemplate.from_template(
     """
-    prompt_here
+    You are a generic agent who answers different questions appropriately.
 
     Question: {input}
     Answer:
