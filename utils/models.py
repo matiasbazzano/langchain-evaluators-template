418801bb0c324ef264dc5277355dca9d5396f8a5
Matias Bazzano <matiasbazzano19@gmail.com>
2025-10-13 16:30:10 -0300

    Update models.py

--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,10 @@
+GPT_5 = "gpt-5"
+GPT_5_MINI = "gpt-5-mini"
+GPT_5_NANO = "gpt-5-nano"
 GPT_4O = "gpt-4o"
 GPT_4O_MINI = "gpt-4o-mini"
 GPT_4_TURBO = "gpt-4-turbo"
 GPT_4 = "gpt-4"
 GPT_35_TURBO = "gpt-3.5-turbo"
 
-DEFAULT_MODEL = GPT_4O_MINI
+DEFAULT_MODEL = GPT_5_MINI
